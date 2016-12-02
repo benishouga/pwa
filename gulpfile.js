f6d0e18ba443a19ba880d8f2d234eca5d03d6372
moriki <moriki>
2016-12-03 04:02:43 +0900

    pwa

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+gulp.task('default', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+  var rootDir = 'app';
+
+  swPrecache.write(path.join(rootDir, 'service-worker.js'), {
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+    stripPrefix: rootDir
+  }, callback);
+});
